8f584fb23e0bef0070f346175499aecff1b91d4f
Jakub Mazanec <jakub@mazanec.info>
2015-03-01 22:09:51 +0100

    Support object fix

--- public/js/app.js
+++ public/js/app.js
@@ -4071,7 +4071,7 @@ module.exports = timeout;
  */
 var support = {};
 
-(function (x) {
+(function () {
 
 	/**
   * Detect if modern javascript is supported.
@@ -4079,7 +4079,12 @@ var support = {};
   * @memberOf ash.support
   * @type boolean
   */
-	support.modernJavascript = typeof Object.getOwnPropertyNames && typeof Object.getPrototypeOf == "function" && typeof Object.defineProperties == "function" && typeof Object.freeze == "function" && typeof Object.freeze == "function" && typeof Function.prototype.bind == "function" && typeof Array.isArray == "function" && { __proto__: [] } instanceof Array && (global.history && global.history.pushState) && global.requestAnimationFrame && global.getComputedStyle;
+	support.modernJavascript = typeof Object.getOwnPropertyNames && typeof Object.getPrototypeOf === "function" && typeof Object.defineProperties === "function" && typeof Object.freeze === "function" && typeof Object.freeze === "function" && typeof Function.prototype.bind === "function" && typeof Array.isArray === "function" && { __proto__: [] } instanceof Array;
+
+	/**
+  */
+
+	support.browser = global.history && global.history.pushState && global.requestAnimationFrame && global.getComputedStyle;
 
 	/**
   * Detect if the DOM is supported.
@@ -4095,12 +4100,14 @@ var support = {};
 })(0, 0);
 
 // add supported class to <html>
-if (support.modernJavascript && support.dom) {
+if (support.modernJavascript && support.browser && support.dom) {
 	global.document.documentElement.className = global.document.documentElement.className.replace(new RegExp("(^|\\b)" + "no-js".split(" ").join("|") + "(\\b|$)", "gi"), " ");
 	global.document.documentElement.className += " js ash--supported";
 	global.document.documentElement.className = global.document.documentElement.className.trim();
-} else {
-	throw new Error("Unsupported javascript version.");
+}
+
+if (!support.modernJavascript) {
+	throw new Error("Unsupported javascript engine.");
 }
 
 module.exports = support;
